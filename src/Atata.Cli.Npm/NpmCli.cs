1f19e37eebf53184f7ba6eaa4ebb74690408e459
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2021-06-24 18:59:17 +0300

    Add source and test files

---
+++ src/Atata.Cli.Npm/NpmCli.cs
@@ -0,0 +1,130 @@
+﻿using System.Text;
+
+namespace Atata.Cli.Npm
+{
+    /// <summary>
+    /// Represents the CLI of "npm" program.
+    /// </summary>
+    public class NpmCli : ProgramCli<NpmCli>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NpmCli"/> class.
+        /// </summary>
+        public NpmCli()
+            : base("npm", true)
+        {
+        }
+
+        /// <summary>
+        /// Ensures NPM is installed.
+        /// If it isn't, throws <see cref="CliCommandException"/>.
+        /// </summary>
+        /// <returns>The same instance.</returns>
+        /// <exception cref="CliCommandException">NPM is not found. Ensure that Node.js and NPM are installed.</exception>
+        public NpmCli EnsureItIsInstalled()
+        {
+            if (!IsItInstalled())
+                throw new CliCommandException("NPM is not found. Ensure that Node.js and NPM are installed.");
+
+            return this;
+        }
+
+        /// <summary>
+        /// Determines whether NPM is installed.
+        /// </summary>
+        /// <returns>
+        /// <see langword="true"/> if it is installed; otherwise, <see langword="false"/>.
+        /// </returns>
+        public bool IsItInstalled() =>
+            string.IsNullOrEmpty(ExecuteRaw("-v").Error);
+
+        /// <summary>
+        /// Determines whether the specified package is installed.
+        /// </summary>
+        /// <param name="packageName">Name of the package.</param>
+        /// <param name="version">The version.</param>
+        /// <param name="global">Is package global.</param>
+        /// <returns>
+        /// <see langword="true"/> if the specified package is installed; otherwise, <see langword="false"/>.
+        /// </returns>
+        public bool IsInstalled(string packageName, string version = null, bool global = false)
+        {
+            packageName.CheckNotNullOrWhitespace(packageName);
+
+            StringBuilder commandText = new StringBuilder("ls");
+
+            AddInstallationParameters(commandText, packageName, version, global);
+
+            commandText.Append(" --depth=0");
+
+            return Execute(commandText.ToString()).ExitCode == 0;
+        }
+
+        /// <summary>
+        /// Installs the specified package.
+        /// </summary>
+        /// <param name="packageName">Name of the package.</param>
+        /// <param name="version">The version.</param>
+        /// <param name="global">Is package global.</param>
+        /// <returns>The same instance.</returns>
+        public NpmCli Install(string packageName, string version = null, bool global = false)
+        {
+            packageName.CheckNotNullOrWhitespace(packageName);
+
+            StringBuilder commandText = new StringBuilder("install");
+
+            AddInstallationParameters(commandText, packageName, version, global);
+
+            Execute(commandText.ToString());
+
+            return this;
+        }
+
+        /// <summary>
+        /// Determines whether the specified package is installed.
+        /// If it isn't, installs the package.
+        /// </summary>
+        /// <param name="packageName">Name of the package.</param>
+        /// <param name="version">The version.</param>
+        /// <param name="global">Is package global.</param>
+        /// <returns>The same instance.</returns>
+        public NpmCli InstallIfMissing(string packageName, string version = null, bool global = false)
+        {
+            if (!IsInstalled(packageName, version, global))
+                Install(packageName, version, global);
+
+            return this;
+        }
+
+        /// <summary>
+        /// Uninstalls the specified package.
+        /// </summary>
+        /// <param name="packageName">Name of the package.</param>
+        /// <param name="version">The version.</param>
+        /// <param name="global">Is package global.</param>
+        /// <returns>The same instance.</returns>
+        public NpmCli Uninstall(string packageName, string version = null, bool global = false)
+        {
+            packageName.CheckNotNullOrWhitespace(packageName);
+
+            StringBuilder commandText = new StringBuilder("uninstall");
+
+            AddInstallationParameters(commandText, packageName, version, global);
+
+            Execute(commandText.ToString());
+
+            return this;
+        }
+
+        private static void AddInstallationParameters(StringBuilder commandText, string packageName, string version, bool global)
+        {
+            if (global)
+                commandText.Append(" -g");
+
+            commandText.Append($" {packageName}");
+
+            if (!string.IsNullOrWhiteSpace(version))
+                commandText.Append($"@{version}");
+        }
+    }
+}
---
+++ test/Atata.Cli.Npm.IntegrationTests/NpmCliTests.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+
+namespace Atata.Cli.Npm.IntegrationTests
+{
+    [TestFixture]
+    public class NpmCliTests
+    {
+        private readonly Subject<NpmCli> _sut = new NpmCli().ToSutSubject();
+
+        [Test]
+        public void IsItInstalled_True()
+        {
+            _sut.ResultOf(x => x.IsItInstalled())
+                .Should.BeTrue();
+        }
+
+        [Test]
+        public void IsInstalledGlobally_True()
+        {
+            _sut.ResultOf(x => x.IsInstalled("npm", null, true))
+                .Should.BeTrue();
+        }
+
+        [Test]
+        public void IsInstalledGlobally_False()
+        {
+            _sut.ResultOf(x => x.IsInstalled("notexistingpackagename", null, true))
+                .Should.BeFalse();
+        }
+
+        [Test]
+        public void Install_True()
+        {
+            string packageName = "lodash";
+
+            EnsureNotInstalledGlobally(packageName);
+
+            _sut.Act(x => x.Install(packageName, null, true));
+
+            _sut.ResultOf(x => x.IsInstalled(packageName, null, true))
+                .Should.BeTrue();
+        }
+
+        [Test]
+        public void Install_False()
+        {
+            string packageName = "notexistingpackagename";
+
+            var exception = Assert.Throws<CliCommandException>(() =>
+                _sut.Act(x => x.Install(packageName, null, true)));
+
+            exception.ToResultSubject()
+                .ValueOf(x => x.Message).Should.Contain(packageName);
+        }
+
+        private void EnsureNotInstalledGlobally(string packageName)
+        {
+            if (_sut.ResultOf(x => x.IsInstalled(packageName, null, true)))
+            {
+                _sut.Act(x => x.Uninstall(packageName, null, true));
+
+                Assume.That(
+                    () => !_sut.ResultOf(x => x.IsInstalled(packageName, null, true)),
+                    $"Failed to uninstall {packageName} package.");
+            }
+        }
+    }
+}
