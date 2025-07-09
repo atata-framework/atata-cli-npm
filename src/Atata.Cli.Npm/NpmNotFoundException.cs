7e9735fed54c06fd9f9da670d2cdd290012ccc93
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-07-09 13:13:54 +0200

    Update `NpmNotFoundException`

--- src/Atata.Cli.Npm/NpmNotFoundException.cs
+++ src/Atata.Cli.Npm/NpmNotFoundException.cs
@@ -3,8 +3,7 @@
 /// <summary>
 /// The exception that is thrown when NPM is not found.
 /// </summary>
-[Serializable]
-public class NpmNotFoundException : Exception
+public sealed class NpmNotFoundException : Exception
 {
     /// <summary>
     /// Initializes a new instance of the <see cref="NpmNotFoundException"/> class.
@@ -29,16 +28,6 @@ public class NpmNotFoundException : Exception
     /// <param name="innerException">The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no inner exception is specified.</param>
     public NpmNotFoundException(string message, Exception innerException)
         : base(message, innerException)
-    {
-    }
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="NpmNotFoundException"/> class.
-    /// </summary>
-    /// <param name="info">The <see cref="SerializationInfo"></see> that holds the serialized object data about the exception being thrown.</param>
-    /// <param name="context">The <see cref="StreamingContext"></see> that contains contextual information about the source or destination.</param>
-    protected NpmNotFoundException(SerializationInfo info, StreamingContext context)
-        : base(info, context)
     {
     }
 }
